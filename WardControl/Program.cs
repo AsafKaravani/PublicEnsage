be595ed6c60dac79714389c00474f3e6b895ac0b
argosptr <argosptr@gmail.com>
2015-12-06 10:26:13 +0700

    fixed a bug : when enemy have dispenser and have more than 1 sentry charges now will draw sentry. Thx to : IdcNoob

--- WardControl/Program.cs
+++ WardControl/Program.cs
@@ -12,19 +12,25 @@ namespace WardControl
     class Program
     {
         static bool adaward = false;
+        static bool wcd = false;
         static Vector3 Position = new Vector3();
         static Vector2 Size = new Vector2(75, 50);
         static String namaward;
         static int[] dispen = new int[5];
+        static int[] dispen2 = new int[5];
         static bool[] inven = new bool[5];
         static bool[] bdispen = new bool[5];
+
+        static List<Hero> ldispen = new List<Hero>();
+        static List<Hero> linven = new List<Hero>();
+
         public static void Main(string[] args)
         {
             Drawing.OnDraw += OnDraw;
             Game.OnUpdate += Game_OnUpdate;
         }
 
-        private static void Game_OnUpdate(EventArgs args)
+        public static void Game_OnUpdate(EventArgs args)
         {
             if (!Game.IsInGame)
                 return;
@@ -33,43 +39,59 @@ namespace WardControl
             List<Hero> enemies = ObjectMgr.GetEntities<Hero>().Where(x => x.Team != me.Team).ToList();
             foreach (Hero enemy in enemies)
             {
-                if (!enemy.IsVisible)
+                if (!enemy.IsVisible && (ldispen.Contains(enemy) || linven.Contains(enemy)))
                 {
-                    inven[index] = false;
-                    bdispen[index] = false;
+                    ldispen.Remove(enemy);
+                    linven.Remove(enemy);
                     dispen[index] = 0;
+                    dispen2[index] = 0;
                     continue;
                 }
+                else if (!enemy.IsVisible)
+                    continue;
                 List<Item> wards = enemy.Inventory.Items.Where(x => x.Name == "item_ward_sentry" || x.Name == "item_ward_observer" || x.Name == "item_ward_dispenser").ToList();
                 if (wards.Any())
                 {
-                    inven[index] = true;
+                    if (!linven.Contains(enemy))
+                        linven.Add(enemy);
                     foreach (Item ward in wards)
                     {
                         if (ward.Name == "item_ward_dispenser")
                         {
-                            bdispen[index] = true;
-                            String sdispen = ward.CurrentCharges.ToString();
-                            if (Convert.ToInt16(sdispen) < dispen[index])
+                            if (!ldispen.Contains(enemy))
+                                ldispen.Add(enemy);
+                            int idispen = Convert.ToInt16(ward.CurrentCharges);
+                            int idispen2 = Convert.ToInt16(ward.SecondaryCharges);
+                            if (idispen < dispen[index])
                                 eksekusi(enemy, "observer");
-                            dispen[index] = Convert.ToInt16(sdispen);
+                            if (idispen2 < dispen2[index])
+                                eksekusi(enemy, "sentry");
+                            dispen[index] = idispen;
+                            dispen2[index] = idispen2;
                         }
                         else if (ward.AbilityState.Equals(AbilityState.OnCooldown))
-                            eksekusi(enemy, ward.Name.Replace("item_ward_", ""));
-                        else if (bdispen[index])
+                        {
+                            if (!wcd)
+                                eksekusi(enemy, ward.Name.Replace("item_ward_", ""));
+                            wcd = true;
+                        }
+                        else if (ldispen.Contains(enemy))
                         {
                             eksekusi(enemy, "dispenser");
-                            bdispen[index] = false;
+                            ldispen.Remove(enemy);
                         }
+                        else
+                            wcd = false; 
+                        
                     }
                 }
-                else if (inven[index])
+                else if (linven.Contains(enemy) && !wards.Any())
                 {
                     eksekusi(enemy, "dispenser");
-                    inven[index] = false;
+                    linven.Remove(enemy);
                 }
                 else
-                    inven[index] = false;
+                    linven.Remove(enemy);
                 index++;
             }
         }
